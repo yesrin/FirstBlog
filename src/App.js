9c303d4ec0bc9ba29f3bdafc5aa9382ee313a2ff
yesrin <asjs11@naver.com>
2021-12-20 21:52:26 +0900

    오류 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import { Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
 import PostPage from './pages/PostPage';
@@ -8,13 +8,16 @@ import WritePage from './pages/WritePage';
 
 function App() {
   return (
-    <>
-      <Route component={PostListPage} path={['/@:username', '/']} />
-      <Route component={LoginPage} path="/login" />
-      <Route component={PostPage} path="/register" />
-      <Route component={WritePage} path="/write" />
-      <Route component={RegisterPage} path="/@username/:PostId" />
-    </>
+    <Router>
+      <Routes>
+        <Route element={<PostListPage />} path="/@:username" exact />
+        <Route element={<PostListPage />} path="/" exact />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<PostPage />} path="/register" />
+        <Route element={<WritePage />} path="/write" />
+        <Route element={<RegisterPage />} path="/@username/:PostId" />
+      </Routes>
+    </Router>
   );
 }
 export default App;
